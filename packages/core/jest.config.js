05c84fbc587aa73aaa86b4cbe44c36229839c1f0
Krystian Drożdżyński <krystian@drozdzynski.me>
2023-08-20 14:12:50 +0200

    Add coverage

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,5 +1,13 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFiles: ['./jest.client.ts']
+  setupFiles: ['./jest.client.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 }
