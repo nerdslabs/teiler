987660a5f86c182c9c27ee03cfb74d2e8caaab6e
Krystian Drożdżyński <krystian@drozdzynski.me>
2023-11-25 20:45:44 +0100

    feat: component as nested selector

--- packages/core/src/constructor.ts
+++ packages/core/src/constructor.ts
@@ -87,29 +87,27 @@ function keyframes(strings: ReadonlyArray<string>, ...properties: Raw[]): StyleD
   }
 }
 
-function insert(sheet: Sheet, definition: StyleDefinition<HTMLElements, unknown>, props: Arguments<unknown>): string {
-  const compiled = compile(definition.styles, props)
-  const rootId = hash(compiled.css)
+function insert(sheet: Sheet, definition: StyleDefinition<HTMLElements, unknown>, props: Arguments<unknown>): string | null {
+  const { styles, type } = definition
+  const { css, definitions } = compile(styles, props)
+  const id = hash(css)
 
-  const definitions = [definition, ...compiled.definitions]
+  if (type === 'component') {
+    const transpiled = transpile(`.teiler-${id} { ${css} }`)
+    sheet.insert(id, transpiled)
 
-  definitions.forEach(({ id, type, styles }) => {
-    const compiled = compile(styles, props)
-    const compiledId = hash(compiled.css)
+    return `teiler-${id}`
+  } else if (type === 'keyframes') {
+    const transpiled = transpile(`@keyframes ${id} { ${css} }`)
+    sheet.insert(id, transpiled)
+  } else {
+    const transpiled = transpile(`${css}`)
+    sheet.insert(id, transpiled)
+  }
 
-    if (type === 'component') {
-      const transpiled = transpile(`.teiler-${compiledId} { ${compiled.css} }`)
-      sheet.insert(compiledId, transpiled)
-    } else if (type === 'keyframes') {
-      const transpiled = transpile(`@keyframes ${id} { ${compiled.css} }`)
-      sheet.insert(compiledId, transpiled)
-    } else {
-      const transpiled = transpile(`${compiled.css}`)
-      sheet.insert(compiledId, transpiled)
-    }
-  })
+  definitions.forEach((definition) => insert(sheet, definition, props))
 
-  return `teiler-${rootId}`
+  return null
 }
 
 export type { Arguments, Compiler, CreateCallback, DefaultTheme, Properties, Sheet, Style, StyleDefinition, TeilerComponent, HTMLElements }
