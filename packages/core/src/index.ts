f093a6865d0fa3d64803a69445b402f29b2339d0
Krystian Drożdżyński <krystian@drozdzynski.me>
2024-01-29 15:51:38 +0100

    Add missing `Pattern` type export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,6 @@
 export type { HTMLElements } from './tags'
 export type { Compiler, DefaultTheme, Properties, Sheet, Style, StyleDefinition, TeilerComponent } from './constructor'
+export type { Pattern } from './pattern'
 
 export { default as tags } from './tags'
 export { default as createStyleSheet } from './sheet'
