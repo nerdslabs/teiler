05ea64cd56fc7d0fcf6a3a8631370516cf139743
Krystian Drożdżyński <krystian@drozdzynski.me>
2024-02-13 12:51:01 +0100

    Fix typing of `ThemeProvider.svelte`

---
+++ packages/svelte/src/ThemeProvider.svelte.d.ts
@@ -0,0 +1,3 @@
+import type { SvelteComponent } from 'svelte';
+import type { DefaultTheme } from '@teiler/core'
+export default class ThemeProvider extends SvelteComponent<{theme: DefaultTheme}> {}
--- packages/svelte/src/index.ts
+++ packages/svelte/src/index.ts
@@ -1,1 +1,2 @@
 export { component, global, keyframes, createComponent } from './component'
+export { default as ThemeProvider } from './ThemeProvider.svelte'
